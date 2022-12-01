a9b0be1c7ff739903049d7d62eff08303cd538f4
Joseph2812 <joemu2@live.com>
2022-12-01 13:41:21 +0000

    Add project files.

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode2022.Puzzles;
+
+namespace AdventOfCode2022
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter a puzzle number: ");
+
+            // Recieve requested puzzle number //
+            int puzzleNumber;
+            if (!int.TryParse(Console.ReadLine(), out puzzleNumber))
+            {
+                Console.WriteLine("Not a valid integer");
+                return;
+            }
+            Console.WriteLine();
+
+            // Puzzle selection //
+            switch (puzzleNumber)
+            {
+                case 1:
+                    new Puzzle1();
+                    break;
+
+                default:
+                    Console.WriteLine($"Puzzle{puzzleNumber} does not exist");
+                    break;
+            }
+        }
+    }
+}
