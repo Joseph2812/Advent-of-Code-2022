cdb000b995696f355646e9cd0d3d92de01dda706
Joseph2812 <joemu2@live.com>
2022-12-01 20:50:15 +0000

    FileStream only open for the required time (Puzzle1)

--- AdventOfCode2022/Puzzles/Puzzle1.cs
+++ AdventOfCode2022/Puzzles/Puzzle1.cs
@@ -4,36 +4,39 @@
     {
         public Puzzle1()
         {
-            using (FileStream fs = new FileStream("Inputs\\Puzzle1.txt", FileMode.Open))
+            // Initialise arrays //
+            string[] itemCaloriesPerElf;
+            using (FileStream fs = new FileStream(@"Inputs\Puzzle1.txt", FileMode.Open))
             {
-                string[] itemCaloriesPerElf = new StreamReader(fs).ReadToEnd().Split("\r\n\r\n"); // {"2000\r\n3000", "1000\r\n3000"}
-                int[] totalCaloriesPerElf = new int[itemCaloriesPerElf.Length];                   // {5000          , 4000          }
-
-                // Add each elf's calorie items up for the total //
-                for (int i = 0; i < itemCaloriesPerElf.Length; i++)
-                {
-                    string[] calories = itemCaloriesPerElf[i].Split("\r\n"); // {"2000", "3000"}
-                    for (int j = 0; j < calories.Length; j++)
-                    {
-                        totalCaloriesPerElf[i] += int.Parse(calories[j]);
-                    }
-                }
-
-                // Find the top 3 calorie holding elfs, and get the grand total //
-                Array.Sort(totalCaloriesPerElf);
-                Console.Write(
-                    "=== Top 3 Calorie Holding Elfs ===\n" +
-                    $"1st: {totalCaloriesPerElf[totalCaloriesPerElf.Length - 1]}\n" +
-                    $"2nd: {totalCaloriesPerElf[totalCaloriesPerElf.Length - 2]}\n" +
-                    $"3rd: {totalCaloriesPerElf[totalCaloriesPerElf.Length - 3]}\n" +
-
-                    string.Format("\nTotal: {0}",
-                        totalCaloriesPerElf[totalCaloriesPerElf.Length - 1] +
-                        totalCaloriesPerElf[totalCaloriesPerElf.Length - 2] +
-                        totalCaloriesPerElf[totalCaloriesPerElf.Length - 3]
-                    )
-                );
+                itemCaloriesPerElf = new StreamReader(fs).ReadToEnd().Split("\r\n\r\n"); // {"2000\r\n3000", "1000\r\n3000"}
             }
+
+            int[] totalCaloriesPerElf = new int[itemCaloriesPerElf.Length]; // {5000, 4000}
+
+            // Add each elf's calorie items up for the total //
+            for (int i = 0; i < itemCaloriesPerElf.Length; i++)
+            {
+                string[] calories = itemCaloriesPerElf[i].Split("\r\n"); // {"2000", "3000"}
+                for (int j = 0; j < calories.Length; j++)
+                {
+                    totalCaloriesPerElf[i] += int.Parse(calories[j]);
+                }
+            }
+
+            // Find the top 3 calorie holding elfs, and get the grand total //
+            Array.Sort(totalCaloriesPerElf);
+            Console.Write(
+                "=== Top 3 Calorie Holding Elfs ===\n" +
+                $"1st: {totalCaloriesPerElf[totalCaloriesPerElf.Length - 1]}\n" +
+                $"2nd: {totalCaloriesPerElf[totalCaloriesPerElf.Length - 2]}\n" +
+                $"3rd: {totalCaloriesPerElf[totalCaloriesPerElf.Length - 3]}\n" +
+
+                string.Format("\nTotal: {0}",
+                    totalCaloriesPerElf[totalCaloriesPerElf.Length - 1] +
+                    totalCaloriesPerElf[totalCaloriesPerElf.Length - 2] +
+                    totalCaloriesPerElf[totalCaloriesPerElf.Length - 3]
+                )
+            );
         }
     }
 }
